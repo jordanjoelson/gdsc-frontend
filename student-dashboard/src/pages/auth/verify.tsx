3dbfc19bc34ccf58b307212543e71716c3ffd8cc
Safa Ahmed <sssafaa.432@gmail.com>
2026-03-02 20:44:32 -0600

    authentication layout

--- student-dashboard/src/pages/auth/verify.tsx
+++ student-dashboard/src/pages/auth/verify.tsx
@@ -1,18 +1,23 @@
 import Authentication from "./Authentication"
+import AuthCard from "./authcard"
 
 export default function Verify() {
   return (
     <Authentication>
-      <div className="text-white text-center">
-        <h1 className="text-2xl font-semibold mb-4">Authentication</h1>
-        <input
-          className="w-full rounded-lg bg-white/80 px-4 py-2 text-[#2b2350] placeholder-[#2b2350]/50 outline-none"
-          placeholder="Enter code"
-        />
-        <button className="mt-5 w-full rounded-xl bg-orange-400 py-2.5 font-semibold text-white">
-          Confirm
-        </button>
-      </div>
+      <AuthCard title="Authentication" showFlowers={false}>
+        
+        <div className="space-y-4 mt-10">
+          <input
+            className="w-full rounded-lg bg-white/80 px-4 py-2 text-[#2b2350] placeholder-[#2b2350]/50 outline-none focus:ring-2 focus:ring-orange-300/60"
+            placeholder="Enter code"
+          />
+
+          <button className="w-full rounded-xl bg-orange-300 py-2.5 font-semibold text-white shadow hover:bg-orange-400 active:opacity-80">
+            Confirm
+          </button>
+        </div>
+
+      </AuthCard>
     </Authentication>
   )
 }
