28736321b386a1baca6d448dbd604ac20e02234d
Safa Ahmed <sssafaa.432@gmail.com>
2026-03-03 16:44:20 -0600

    added forgot password pages

--- student-dashboard/src/pages/auth/verify.tsx
+++ student-dashboard/src/pages/auth/verify.tsx
@@ -7,12 +7,14 @@ export default function Verify() {
       <AuthCard title="Authentication" showFlowers={false}>
         
         <div className="space-y-4 mt-10">
+          <p className = "text-white/60 text-center mb-10">Enter the code sent to your email</p>
+          
           <input
             className="w-full rounded-lg bg-white/80 px-4 py-2 text-[#2b2350] placeholder-[#2b2350]/50 outline-none focus:ring-2 focus:ring-orange-300/60"
             placeholder="Enter code"
           />
 
-          <button className="w-full rounded-xl bg-orange-300 py-2.5 font-semibold text-white shadow hover:bg-orange-400 active:opacity-80">
+          <button className="w-full rounded-xl bg-orange-300 py-2.5 font-semibold text-white shadow hover:bg-orange-400 active:opacity-80 hover:cursor-pointer transition duration-300">
             Confirm
           </button>
         </div>
