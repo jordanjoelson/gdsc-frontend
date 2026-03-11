19e3f8af53f24754940e927faa3208f1026f9907
Sanjit Kadam <164111429+Sanjitkadam1@users.noreply.github.com>
2026-03-11 15:23:05 -0500

    Updated Layout.tsx to work with the modern <Outlet> instead of children

--- student-dashboard/src/app/layout.tsx
+++ student-dashboard/src/app/layout.tsx
@@ -1,11 +1,11 @@
 import { SidebarProvider } from "@/components/ui/sidebar"
 import { UserDropdown } from "@/components/UserDropdown"
 import { AppSidebar } from "@/components/app-sidebar"
-import { useLocation } from "react-router-dom"
+import { useLocation, Outlet } from "react-router-dom"
 
 //This is the layout page, it is where everything that will stay on the page regardless of the rest of the inside app
 
-export default function Layout({ children }: { children: React.ReactNode }) {
+export default function Layout() {
   const location = useLocation();
   const hideSidebar = location.pathname === "/settings";
 
@@ -14,9 +14,7 @@ export default function Layout({ children }: { children: React.ReactNode }) {
       {!hideSidebar && <AppSidebar />}
       <main className="flex-1">
         <div
-          className="flex items-center justify-end gap-4 p-4 bg-[#352D51]"
-          style={{ boxShadow: "none", border: "none" }}
-        >
+          className="flex items-center justify-end gap-4 p-4 bg-[#352D51]" style={{ boxShadow: "none", border: "none" }}>
           <UserDropdown />
         </div>
 
