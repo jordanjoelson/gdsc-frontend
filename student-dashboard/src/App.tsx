c3a5dfbb50f5b2c1f04b5339977e1912c522ef6b
Safa Ahmed <sssafaa.432@gmail.com>
2026-03-10 23:02:06 -0500

    added routing

--- student-dashboard/src/App.tsx
+++ student-dashboard/src/App.tsx
@@ -5,7 +5,7 @@ import './style.css'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import Layout from "@/app/layout"
 
-//pages being imported
+// pages
 import Dashboard from "@/pages/Dashboard"
 import Classes from "@/pages/Classes"
 import Calendar from "@/pages/Calendar"
@@ -14,28 +14,51 @@ import CourseTasksPage from "@/pages/CourseTasksPage"
 import SettingsPage from "@/pages/SettingsPage"
 import AllTasks from "@/pages/AllTasks"
 
+// auth pages
+import Landing from "@/pages/authentication/landing"
+import Login from "@/pages/authentication/login"
+import Signup from "@/pages/authentication/signup"
+import EnterEmail from "@/pages/authentication/enterEmail"
+import EnterCode from "@/pages/authentication/enterCode"
+import ResetPassword from "@/pages/authentication/resetPassword"
 
 export default function App() {
   return (
     <Router>
-      <Layout>
-        <Routes>
-          <Route path="/" element={<Dashboard />} />
-          <Route path="/classes" element={<Classes />} />
-          <Route path="/classes/:courseId" element={<CourseTasksPage />} />
-          <Route path="/calendar" element={<Calendar />} />
-          <Route path="/achievments" element={<Achievements />} />
-          <Route path="/settings" element={<SettingsPage />} />
-          <Route path="/allTasks" element={<AllTasks />} />
-        </Routes>
-      </Layout>
+      <Routes>
+
+        {/* Auth Pages (no sidebar layout) */}
+        <Route path="/landing" element={<Landing />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/enter-email" element={<EnterEmail />} />
+        <Route path="/enter-code" element={<EnterCode />} />
+        <Route path="/reset-password" element={<ResetPassword />} />
+
+        {/* Dashboard Layout Pages */}
+        <Route
+          path="/*"
+          element={
+            <Layout>
+              <Routes>
+                <Route path="/" element={<Dashboard />} />
+                <Route path="/classes" element={<Classes />} />
+                <Route path="/classes/:courseId" element={<CourseTasksPage />} />
+                <Route path="/calendar" element={<Calendar />} />
+                <Route path="/achievements" element={<Achievements />} />
+                <Route path="/settings" element={<SettingsPage />} />
+                <Route path="/allTasks" element={<AllTasks />} />
+              </Routes>
+            </Layout>
+          }
+        />
+
+      </Routes>
     </Router>
   )
 }
 
-
-
-//DON'T TOUCH, this makes react able to put things directly into the website DOM
+// DON'T TOUCH
 ReactDOM.createRoot(document.getElementById('app')!).render(
   <React.StrictMode>
     <App />
