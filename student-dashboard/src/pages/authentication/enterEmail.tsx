c48a9f28ba3df8f4b0a2dea272595e0d5e4e1d02
Safa Ahmed <sssafaa.432@gmail.com>
2026-03-10 22:41:13 -0500

    created resetting passwords page

---
+++ student-dashboard/src/pages/authentication/enterEmail.tsx
@@ -0,0 +1,38 @@
+import { useNavigate } from "react-router-dom"
+import Authentication from "./auth"
+import AuthCard from "./authCard"
+
+export default function Verify() {
+
+  const navigate = useNavigate()
+
+  const handleConfirm = () => {
+    navigate("/resetPassword")
+  }
+
+  return (
+    <Authentication>
+      <AuthCard title="Authentication" showFlowers={false}>
+        
+        <div className="space-y-4 mt-10">
+          <p className="text-white/60 text-center mb-10">
+            Enter your email address and we'll send you a code to reset your password
+          </p>
+          
+          <input
+            className="w-full rounded-lg bg-white/80 px-4 py-2 text-[#2b2350] placeholder-[#2b2350]/50 outline-none focus:ring-2 focus:ring-orange-300/60"
+            placeholder="Enter email"
+          />
+
+          <button
+            onClick={handleConfirm}
+            className="w-full rounded-xl bg-orange-300 py-2.5 font-semibold text-white shadow hover:bg-orange-400 active:opacity-80 hover:cursor-pointer transition duration-300"
+          >
+            Confirm
+          </button>
+        </div>
+
+      </AuthCard>
+    </Authentication>
+  )
+}
---
+++ student-dashboard/src/pages/authentication/resetPassword.tsx
@@ -0,0 +1,56 @@
+import { useState } from "react"
+import { useNavigate } from "react-router-dom"
+import Authentication from "./auth"
+import AuthCard from "./authCard"
+
+export default function Verify() {
+
+  const [password, setPassword] = useState("")
+  const [confirmPassword, setConfirmPassword] = useState("")
+  const navigate = useNavigate()
+
+  const handleConfirm = () => {
+    if (password === confirmPassword && password !== "") {
+      navigate("/app") // goes to dashboard
+    } else {
+      alert("Passwords do not match")
+    }
+  }
+
+  return (
+    <Authentication>
+      <AuthCard title="Reset Password" showFlowers={false}>
+        
+        <div className="space-y-4 mt-10">
+          <p className="text-white/60 text-center mb-10">
+            Enter your new password
+          </p>
+          
+          <input
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            className="w-full rounded-lg bg-white/80 px-4 py-2 text-[#2b2350] placeholder-[#2b2350]/50 outline-none focus:ring-2 focus:ring-orange-300/60"
+            placeholder="Enter new password"
+            type="password"
+          />
+
+          <input
+            value={confirmPassword}
+            onChange={(e) => setConfirmPassword(e.target.value)}
+            className="w-full rounded-lg bg-white/80 px-4 py-2 text-[#2b2350] placeholder-[#2b2350]/50 outline-none focus:ring-2 focus:ring-orange-300/60"
+            placeholder="Confirm new password"
+            type="password"
+          />
+
+          <button
+            onClick={handleConfirm}
+            className="w-full rounded-xl bg-orange-300 py-2.5 font-semibold text-white shadow hover:bg-orange-400 active:opacity-80 hover:cursor-pointer transition duration-300"
+          >
+            Confirm
+          </button>
+        </div>
+
+      </AuthCard>
+    </Authentication>
+  )
+}
