0b37ba8a8fd16d6670d0084b5770c9e9301bb316
Safa Ahmed <sssafaa.432@gmail.com>
2026-03-11 19:11:03 -0500

    fixed spacing of courses and horizontal bar

--- student-dashboard/src/pages/Dashboard.tsx
+++ student-dashboard/src/pages/Dashboard.tsx
@@ -49,7 +49,7 @@ export default function Dashboard() {
     .sort((a, b) => new Date(a.due).getTime() - new Date(b.due).getTime())
 
   return (
-    <section className="p-10 min-h-screen bg-[#352D51]">
+    <section className="pt-10 pr-10 pb-10 min-h-screen bg-[#352D51]">
 
       {/* Welcome */}
       <h1 className="text-white text-6xl font-bold mb-12 -mt-15">
@@ -59,7 +59,6 @@ export default function Dashboard() {
       {/* UPCOMING TASKS */}
       <div className="mb-16">
 
-        {/* Title + View All */}
         <div className="flex items-center justify-between w-120 ml-10 mb-13">
           <h2 className="text-white text-4xl font-semibold">
             Upcoming Tasks
@@ -75,19 +74,14 @@ export default function Dashboard() {
 
         <div className="relative group w-120 ml-10">
 
-          {/* orange line horizontal */}
           <div className="absolute -top-8 left-7 w-4/5 h-2 bg-orange-300 rounded-full z-0"></div>
 
-          {/* orange line vertical */}
           <div className="absolute -left-8 top-4 w-2 h-4/5 bg-orange-300 rounded-full" />
 
-          {/* Back card */}
           <div className="absolute inset-0 -translate-x-4 -translate-y-4 rounded-xl bg-[#1F1C3D]" />
 
-          {/* Outer card */}
           <div className="relative z-10 rounded-xl bg-[#140D2A] p-3">
 
-            {/* Scroll container */}
             <div className="scrollbar h-64 overflow-y-auto rounded-lg">
 
               {upcomingTasks.map((task) => (
@@ -119,9 +113,8 @@ export default function Dashboard() {
       {/* COURSES */}
       <div className="mt-10">
 
-        {/* Title + View all */}
         <div className="flex items-center justify-between mb-6">
-          <h2 className="text-white text-4xl font-semibold">
+          <h2 className="text-white text-4xl font-semibold ml-15">
             Courses
           </h2>
 
@@ -133,14 +126,12 @@ export default function Dashboard() {
           </Link>
         </div>
 
-        {/* CARD WRAPPER */}
         <div className="relative w-full max-w-300">
-          
-          {/* main container */}
+
           <div className="relative z-10">
 
-            {/* HORIZONTAL SCROLL AREA */}
-            <div className="scrollbar flex gap-14 overflow-x-auto pb-10 px-6 pt-8 pr-70">
+            {/* HORIZONTAL SCROLL */}
+            <div className="scrollbar flex gap-14 overflow-x-auto pb-10 pt-8 pr-60 pl-20">
 
               {courses.map((course) => (
                 <div key={course.id} className="shrink-0 w-100">
